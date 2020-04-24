1a512b0c39450bd2e3ac549721449498a9cfbff9
jshsam <49328153+jshsam@users.noreply.github.com>
2020-04-24 14:32:29 -0400

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,50 +1,29 @@
 
+/**
+	Class Book represents a book for sale at a book store.
+	This version of the class is a subclass of class BookStoreItem
+	and inherits variables and methods from it. 
+	Author: James Hembree
+	E-mail address: jshembree88@gmail.com
+	Last changed: April 24, 2020
+*/
 
 public class Book extends BookStoreItem {
 	
-	private int pages;
-	
-	public Book (String title, String author, double price) { //, int pages) {
+	public Book (String title, String author, double price) {
 		super(title, author, price);
 	}
 	
 	public Book () {
-		//call the other constructor and pass it a generic title, author and price
+		//call the other constructor and pass it a generic  title, author and price
 		this("title", "author", 0.0);
-	}
-
-	public int getPages() {
-		return this.pages;
-	}
-	
-	public String getSize() {
-		String size;
-		
-		size = this.pages + " pages";
-		
-		return size;
 	}
 	
 	public String toString() {
 		String book;
 		book = "Book:\n" + super.toString();  
-		return book;
-	}
 	
-	public boolean equals(Object obj) {
-		boolean equalBooks;
-			
-		equalBooks = super.equals(obj);
-		
-		if (equalBooks) {
-			if (obj instanceof Book) {
-				if (this.pages != ((Book)obj).pages) {
-					equalBooks = false;
-				}
-			}
-		}
-		
-		return equalBooks;
+		return book;
 	}
 		
 }
